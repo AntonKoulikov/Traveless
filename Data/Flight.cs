dab9db0a8b855e29f16f76e8c2a141a0835589b1
AntonKoulikov <117700254+AntonKoulikov@users.noreply.github.com>
2023-03-16 11:45:46 -0600

    Add project files.

---
+++ Data/Flight.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traveless.Data
+{
+    internal class Flight
+    {
+        private string flightCode;
+        private string airline;
+        private string from;
+        private string to;
+        private string day;
+        private string time;
+        private int cost;
+
+        public string FlightCode { get; set; }
+        public string Airline { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Day { get; set; }
+        public string Time { get; set; }
+        public int Cost { get; set; }
+
+        public Flight() { }
+
+        public Flight (string flightCode, string airline, string from, string to, string day, string time, int cost)
+        {
+            FlightCode = flightCode;
+            Airline = airline;
+            From = from;
+            To = to;
+            Day = day;
+            Time = time;
+            Cost = cost;
+        }
+    }
+}
---
+++ Data/FlightManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traveless.Data
+{
+    internal class FlightManager
+    {
+        private const string FlightsFile = @"C:\Users\Anton\source\repos\Traveless\Resources\Files\flights.csv";
+        public static List<Flight> flights = new List<Flight>();
+
+        public FlightManager()
+        {
+            populateFlights();
+        }
+
+        private void populateFlights()
+        {
+            Flight flight;
+
+            foreach(string csvLine in System.IO.File.ReadLines(FlightsFile))
+            {
+                string[] cells = csvLine.Split(",");
+
+                string flightCode = cells[0];
+                string airline = cells[1];
+                string from = cells[2];
+                string to = cells[3];
+                string day = cells[4];
+                string time = cells[5];
+                int cost = int.Parse(cells[6]);
+
+                flight = new Flight(flightCode, airline, from, to, day, time, cost);
+                flights.Add(flight);
+            }
+        }
+
+        public static List<Flight> GetFlights()
+        {
+            return flights;
+        }
+    }
+}
